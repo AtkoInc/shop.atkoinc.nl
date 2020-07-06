aa000a111b9fa1b1e98d84e1fe8bc55bf4c9bcbf
Frank Benus <benusfrank@gmail.com>
2020-07-06 12:56:28 +0200

    Authcode with PKCE implemented

--- assets/js/general.js
+++ assets/js/general.js
@@ -1,13 +1,4 @@
 // ------------------------------------------------------------------------------------------
-// Global parameters
-// ------------------------------------------------------------------------------------------
-	var oktaOrgUrl = 'https://atkoshop.oktapreview.com';
-	localStorage.setItem('oktaurl', oktaOrgUrl);
-	localStorage.setItem('portalurl', 'http://localhost:8000');
-	localStorage.setItem('logging', true);
-	
-
-// ------------------------------------------------------------------------------------------
 // generic functions
 // ------------------------------------------------------------------------------------------
 
@@ -18,19 +9,37 @@ function writeLog(message) {
 	} 
 }
 
+function getParameterByName( name ) //courtesy Artem
+{
+  name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+  var regexS = "[\\?&]"+name+"=([^&#]*)";
+  var regex = new RegExp( regexS );
+  var results = regex.exec( window.location.href );
+  if( results == null )
+    return "";
+  else
+    return decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
 // ------------------------------------------------------------------------------------------
 // session management
 // ------------------------------------------------------------------------------------------
 
+	function getAuthorisationCode(codeChallenge) {
+		var codeUrl = localStorage.getItem('oktaurl') + '/oauth2/'+ localStorage.getItem('authorizationserver') +'/v1/authorize?client_id='+ localStorage.getItem('clientid')  +'&response_type=code&scope=openid&redirect_uri='+localStorage.getItem('portalurl')+'/callback&state=x&nonce=y&code_challenge_method=S256&code_challenge='+ codeChallenge
+		window.location = codeUrl
+	}
+
 	function checkSession() {
 		$.ajax({
-			url: oktaOrgUrl + '/api/v1/users/me',
+			url: localStorage.getItem('oktaurl') + '/api/v1/users/me',
 			type: 'GET',
 			xhrFields: { withCredentials: true },
 			accept: 'application/json'
 		}).done(function (response) {
 			localStorage.setItem('user', response.id)
 		    writeLog('-> result function: checkSession = done with id '+ response.id);   
+		    writeLog(response)
 		    return response
 		}).fail(function(response) {
 		    writeLog('-> result function: checkSession = fail');   
