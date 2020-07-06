c4b4a25d27c30f3d67e282c5db5ebc17a8e63c26
Frank Benus <benusfrank@gmail.com>
2020-07-06 16:54:23 +0200

    Finishing up auth-n flow

--- assets/js/general.js
+++ assets/js/general.js
@@ -21,28 +21,55 @@ function getParameterByName( name ) //courtesy Artem
     return decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
+function parseJwt(token) {
+    if (token) {
+	    var base64Url = token.split('.')[1];
+	    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+	    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+	        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+	    }).join(''));
+	    return JSON.parse(jsonPayload);
+    } else {
+    	return false;
+    }
+
+    
+};
+
 // ------------------------------------------------------------------------------------------
 // session management
 // ------------------------------------------------------------------------------------------
 
 	function getAuthorisationCode(codeChallenge) {
-		var codeUrl = localStorage.getItem('oktaurl') + '/oauth2/'+ localStorage.getItem('authorizationserver') +'/v1/authorize?client_id='+ localStorage.getItem('clientid')  +'&response_type=code&scope=openid&redirect_uri='+localStorage.getItem('portalurl')+'/callback&state=x&nonce=y&code_challenge_method=S256&code_challenge='+ codeChallenge
+		var codeUrl = localStorage.getItem('oktaurl') + '/oauth2/'+ localStorage.getItem('authorizationserver') +'/v1/authorize?client_id='+ localStorage.getItem('clientid')  +'&response_type=code&scope='+ localStorage.getItem('scopes') +'&redirect_uri='+localStorage.getItem('portalurl')+'/callback&state=x&nonce=y&code_challenge_method=S256&code_challenge='+ codeChallenge
 		window.location = codeUrl
 	}
 
-	function checkSession() {
-		$.ajax({
-			url: localStorage.getItem('oktaurl') + '/api/v1/users/me',
-			type: 'GET',
-			xhrFields: { withCredentials: true },
-			accept: 'application/json'
-		}).done(function (response) {
-			localStorage.setItem('user', response.id)
-		    writeLog('-> result function: checkSession = done with id '+ response.id);   
-		    writeLog(response)
-		    return response
-		}).fail(function(response) {
-		    writeLog('-> result function: checkSession = fail');   
-			return  false
-		})
+	function validateToken(tokenType) {
+		var raw_idToken = localStorage.getItem(tokenType);
+		var expValidity = false
+
+		if (raw_idToken) {
+			var jwt_idToken = parseJwt(raw_idToken)
+			// validate exp
+			var currentTimeStamp = Date.now() / 1000
+			var expValidity = jwt_idToken.exp > currentTimeStamp
+			
+			writeLog('now: '+currentTimeStamp);
+			writeLog('jwt expires: '+jwt_idToken.exp);
+
+			writeLog('-> token validity = ' + expValidity +' (for user: '+ jwt_idToken.name +')')
+			// store data in localStorage
+			var jwt_id_name = jwt_idToken.name;
+			writeLog(jwt_idToken);
+			localStorage.setItem('jwt_id_name', jwt_id_name);
+
+		} else {
+			writeLog('some shit went down');
+		}
+		return expValidity;
+	}
+
+	function trashToken(tokenType) {
+		localStorage.removeItem(tokenType);
 	}
