813d1a19bef4bf6f1dd303e255ea1d5dc0b39e2e
Frank Benus <benusfrank@gmail.com>
2020-07-15 08:38:08 +0200

    changed how browser opens

--- assets/js/general.js
+++ assets/js/general.js
@@ -56,6 +56,53 @@ function processMessage(message) {
     }
 }
 
+function openUrl(url, readerMode) {
+  if (typeof youSafariViewControllerrvar !== 'undefined') {
+      writeLog('-> SafariViewController is defined')
+	  SafariViewController.isAvailable(function (available) {
+	    if (available) {
+	      writeLog('-> SafariViewController is available')
+	      SafariViewController.show({
+	            url: url,
+	            hidden: false, // default false. You can use this to load cookies etc in the background (see issue #1 for details).
+	            animated: false, // default true, note that 'hide' will reuse this preference (the 'Done' button will always animate though)
+	            transition: 'curl', // (this only works in iOS 9.1/9.2 and lower) unless animated is false you can choose from: curl, flip, fade, slide (default)
+	            enterReaderModeIfAvailable: readerMode, // default false
+	            tintColor: "#00ffff", // default is ios blue
+	            barColor: "#0000ff", // on iOS 10+ you can change the background color as well
+	            controlTintColor: "#ffffff" // on iOS 10+ you can override the default tintColor
+	          },
+	          // this success handler will be invoked for the lifecycle events 'opened', 'loaded' and 'closed'
+	          function(result) {
+	            if (result.event === 'opened') {
+	              console.log('opened');
+	            } else if (result.event === 'loaded') {
+	              console.log('loaded');
+	            } else if (result.event === 'closed') {
+	              console.log('closed');
+	            }
+	          },
+	          function(msg) {
+	            console.log("KO: " + msg);
+	          })
+	    } else {
+	      // potentially powered by InAppBrowser because that (currently) clobbers window.open
+	      writeLog('-> SafariViewController is not available')
+	      window.open(url, '_blank', 'location=yes');
+	    }
+	  })			  
+	} else {
+      writeLog('-> SafariViewController is not defined')
+      window.open(url);
+  }
+
+}
+ 
+function dismissSafari() {
+  SafariViewController.hide()
+}
+
+
 // ------------------------------------------------------------------------------------------
 // session management
 // ------------------------------------------------------------------------------------------
@@ -63,7 +110,7 @@ function processMessage(message) {
 	function getAuthorisationCode(codeChallenge) {
 		var codeUrl = localStorage.getItem('oktaurl') + '/oauth2/'+ localStorage.getItem('authorizationserver') +'/v1/authorize?client_id='+ localStorage.getItem('clientid')  +'&response_type=code&scope='+ localStorage.getItem('scopes') +'&redirect_uri='+localStorage.getItem('portalcallbackurl')+'&state=x&nonce=y&code_challenge_method=S256&code_challenge='+ codeChallenge
 		alert('-> getting an authorisation here: '+ codeUrl);
-		window.location = codeUrl
+		openUrl(codeUrl, false) 
 	}
 
 	function getTokensWithCode(authorisationCode) {
