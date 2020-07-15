6d339bbb1149450d8f11b937d11df1489bf570fd
Frank Benus <benusfrank@gmail.com>
2020-07-15 09:12:36 +0200

    Fix basics

--- assets/js/general.js
+++ assets/js/general.js
@@ -59,45 +59,42 @@ function processMessage(message) {
 function openUrl(url, readerMode) {
   if (typeof youSafariViewControllerrvar !== 'undefined') {
       alert('-> SafariViewController is defined')
-	  SafariViewController.isAvailable(function (available) {
-	    if (available) {
-	      writeLog('-> SafariViewController is available')
-	      SafariViewController.show({
-	            url: url,
-	            hidden: false, // default false. You can use this to load cookies etc in the background (see issue #1 for details).
-	            animated: false, // default true, note that 'hide' will reuse this preference (the 'Done' button will always animate though)
-	            transition: 'curl', // (this only works in iOS 9.1/9.2 and lower) unless animated is false you can choose from: curl, flip, fade, slide (default)
-	            enterReaderModeIfAvailable: readerMode, // default false
-	            tintColor: "#00ffff", // default is ios blue
-	            barColor: "#0000ff", // on iOS 10+ you can change the background color as well
-	            controlTintColor: "#ffffff" // on iOS 10+ you can override the default tintColor
-	          },
-	          // this success handler will be invoked for the lifecycle events 'opened', 'loaded' and 'closed'
-	          function(result) {
-	            if (result.event === 'opened') {
-	              console.log('opened');
-	            } else if (result.event === 'loaded') {
-	              console.log('loaded');
-	            } else if (result.event === 'closed') {
-	              console.log('closed');
-	            }
-	          },
-	          function(msg) {
-	            console.log("KO: " + msg);
-	          })
-	    } else {
-	      // potentially powered by InAppBrowser because that (currently) clobbers window.open
-	      alert('-> SafariViewController is not available')
-	      window.open(url, '_blank', 'location=yes');
-	    }
-	  })			  
-	} else {
+      SafariViewController.isAvailable(function (available) {
+      if (available) {
+        writeLog('-> SafariViewController is available')
+        SafariViewController.show({
+              url: url,
+              hidden: false, // default false. You can use this to load cookies etc in the background (see issue #1 for details).
+              animated: false, // default true, note that 'hide' will reuse this preference (the 'Done' button will always animate though)
+              transition: 'curl', // (this only works in iOS 9.1/9.2 and lower) unless animated is false you can choose from: curl, flip, fade, slide (default)
+              enterReaderModeIfAvailable: readerMode, // default false
+              tintColor: "#00ffff", // default is ios blue
+              barColor: "#0000ff", // on iOS 10+ you can change the background color as well
+              controlTintColor: "#ffffff" // on iOS 10+ you can override the default tintColor
+            },
+            // this success handler will be invoked for the lifecycle events 'opened', 'loaded' and 'closed'
+            function(result) {
+              if (result.event === 'opened') {
+                console.log('opened');
+              } else if (result.event === 'loaded') {
+                console.log('loaded');
+              } else if (result.event === 'closed') {
+                console.log('closed');
+              }
+            },
+            function(msg) {
+              console.log("KO: " + msg);
+            })
+      } else {
+        // potentially powered by InAppBrowser because that (currently) clobbers window.open
+        alert('-> SafariViewController is not available')
+        window.open(url, '_blank', 'location=yes');
+      }
+    })        
+  } else {
       alert('-> SafariViewController is not defined')
       window.open(url, '_self', 'location=yes');
-  }
-
-}
- 
+  } 
 function dismissSafari() {
   SafariViewController.hide()
 }
